15edbafce825dc1de1cdfc9f9e280723d0df108f
justcallmelee10 <g96hjtnf2n@privaterelay.appleid.com>
2026-05-29 18:09:27 +0300

    main.py

--- main.py
+++ main.py
@@ -1,26 +1,30 @@
+import requests
 import time
-import random
 
-def get_market_data():
-    # fake market simulation for now (we upgrade later to real forex feed)
-    return {
-        "price": round(random.uniform(1.05, 1.15), 5)
-    }
+API_KEY = "2UFIT1549JNPNLPY"
+
+def get_price():
+    url = f"https://www.alphavantage.co/query?function=CURRENCY_EXCHANGE_RATE&from_currency=EUR&to_currency=USD&apikey={API_KEY}"
+    data = requests.get(url).json()
+    
+    price = float(data["Realtime Currency Exchange Rate"]["5. Exchange Rate"])
+    return price
 
 def strategy(price):
     if price > 1.10:
-        return "SELL", "Price above threshold (overbought zone)"
+        return "SELL", "High zone detected"
     elif price < 1.08:
-        return "BUY", "Price below threshold (value zone)"
-    else:
-        return "NO TRADE", "Market neutral"
+        return "BUY", "Low zone detected"
+    return "NO TRADE", "Neutral zone"
 
-while True:
-    data = get_market_data()
-    signal, reason = strategy(data["price"])
+print("REAL MARKET BOT STARTED")
 
-    print("PRICE:", data["price"])
+for i in range(20):
+    price = get_price()
+    signal, reason = strategy(price)
+
+    print("PRICE:", price)
     print("SIGNAL:", signal)
     print("REASON:", reason)
 
-    time.sleep(5)
+    time.sleep(10)
